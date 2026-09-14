c212fbd58aadb18f8d40619cf5f7a0649120b941
gopalamPraneeth <gopalampraneeth369@gmail.com>
2026-09-14 22:49:49 +0530

    Initial project upload

---
+++ models/student.py
@@ -0,0 +1,38 @@
+from utils.file_handler import FileHandler
+
+class ResultManager:
+    def __init__(self):
+        self.students = []
+        self.file_handler = FileHandler()
+    def add_student (self, student: Student):
+        #TODO: Add student to list
+        pass
+    def get_all_students (self):
+        #TODO: Return all students
+        pass
+    def find_topper(self):
+        #TODO: Return student with highes
+        pass
+    def load_students (self):
+        #TODO: Load students from file
+        pass
+    def save_students (self):
+        #TODO: Save students to file
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
